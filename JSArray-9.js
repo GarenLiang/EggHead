4b019696fc3dd2545b179df64ec2758d1f3f56cd
Garen Liang <ideabreeze@gmail.com>
2017-02-13 23:41:36 -0800

    Create JSArray-9.js

---
+++ JSArray-9.js
@@ -0,0 +1,30 @@
+// Array.prototype.map
+
+const items = [
+{
+active: true,
+firstname: 'Shane',
+lastname: 'Osbourne'
+},
+{
+active: true,
+firstname: 'Sally',
+lastname: 'Osbourne'
+},
+{
+active: false,
+firstname: 'Ben',
+lastname: 'Barker'
+}
+];
+
+const mapped = items
+.filter(x => x.active) //
+.map(x => x.firstname); // ['Shane', 'Sally']
+
+console.log(createHtmlList(mapped));
+
+function createHtmlList(items) {
+const listElements = items.map(x => <li>${x}</li>).join('\n');
+return <ul>\n${listElements}\n</ul>;
+}
